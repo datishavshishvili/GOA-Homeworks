d5ca277f418d737dc2b23caad26057411cba2ae4
datishavshishvili <datitsertsvadze0@gmail.com>
2024-02-25 19:44:04 +0400

    second commit

---
+++ Day 1/homework.py
@@ -0,0 +1,7 @@
+# We will now declare a variable of my name, age and surname
+
+name = "dati"
+surname = "tserstvadze"
+age = 16
+
+print(" My name is " + name + " and my surname is " + surname + " and my age is " + str(age) )
